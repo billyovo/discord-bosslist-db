cd28e57580e527e6052d60f713ce64b270218c5d
billyovo <30436689+billyovo@users.noreply.github.com>
2021-03-03 10:23:22 +0800

    Update index.js

--- index.js
+++ index.js
@@ -182,7 +182,6 @@ const host = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const requestListener = function (req, res) {
-    console.log(req.destination);
     if(req.destination!==undefined){
       res.writeHead(404);
       res.end();
