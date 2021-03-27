aa327065a94d5bd13860c01fa213d461ae353104
billyovo <30436689+billyovo@users.noreply.github.com>
2021-03-27 14:05:17 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -87,7 +87,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
  
 app.head ("/availability", function (req, res) {  //availability or keep awake
-  res.sendStatus(200);
+  db.any('SELECT player.*, boss01.boss AS boss1, boss01.hitted AS hitted1, boss02.boss AS boss2, boss02.hitted AS hitted2 FROM player INNER JOIN boss01 ON player.name = boss01.name INNER JOIN boss02 ON player.name = boss02.name', [true])
+    .then(function(data) {
+        res.status(200).send(JSON.stringify(data));
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
 }) 
 
 app.get('/players', function (req, res) {  //get records
