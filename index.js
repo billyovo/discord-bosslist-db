c7479292afea94797eec3b9ed818dacb963c9c6a
billyovo <30436689+billyovo@users.noreply.github.com>
2021-03-28 13:06:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
  
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.ACCESS_ORIGIN);
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
