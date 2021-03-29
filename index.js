f33a0fe39f4f1e1065bc0cd47716efa16432c1be
billyovo <30436689+billyovo@users.noreply.github.com>
2021-03-29 11:22:15 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -385,6 +385,7 @@ app.use(bodyParser.json());
 app.all('/*', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", process.env.ACCESS_ORIGIN);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods","GET, HEAD, POST, PATCH");
   next();
 });
 
