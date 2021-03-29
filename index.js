57fa86230de0fdc77985769133221970ec46cd71
billyovo <30436689+billyovo@users.noreply.github.com>
2021-03-29 14:55:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -274,7 +274,7 @@ bot.on('message',async (msg) => {
         F: [],
         G: [],
       }
-      let res = await client.query('SELECT player.*, boss01.boss AS boss1, boss01.hitted AS hitted1, boss02.boss AS boss2, boss02.hitted AS hitted2 FROM player INNER JOIN boss01 ON player.name = boss01.name INNER JOIN boss02 ON player.name = boss02.name;');
+      let res = await client.query('SELECT player.*, boss01.boss AS boss1, boss01.hitted AS hitted1, boss02.boss AS boss2, boss02.hitted AS hitted2 FROM player INNER JOIN boss01 ON player.name = boss01.name INNER JOIN boss02 ON player.name = boss02.name ORDER BY player.name;');
       res.rows.forEach(element=>{
         result[element.boss1].push(element.name);
         result[element.boss2].push(element.name);
